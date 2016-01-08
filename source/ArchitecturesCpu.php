95b7d01d04d51eaf64b71edbf656914193487d70
Daniel Popiniuc <danielpopiniuc@gmail.com>
2016-01-08 15:54:07 +0200

    received a couple of functions + internal optimization for easier maintenance of code

--- source/ArchitecturesCpu.php
+++ source/ArchitecturesCpu.php
@@ -29,55 +29,130 @@ namespace danielgp\browser_agent_info;
 trait ArchiecturesCpu
 {
 
+    private function addStdandardTrunk($inArray, $trnkArray)
+    {
+        $outArray = [];
+        foreach ($inArray as $key => $value) {
+            $value2         = array_merge($value, $trnkArray);
+            ksort($value2);
+            $outArray[$key] = $value2;
+        }
+        return $outArray;
+    }
+
+    private function detectX64Architecture($usrA)
+    {
+        $idn      = ['amd64;', 'AMD64', 'x86_64;'];
+        $byPatern = ($this->eVlByAltSubstrings($idn, $usrA, 'ia32') === 'ia32');
+        $sReturn  = '';
+        if ($byPatern || (strpos($usrA, 'Win64;') && strpos($usrA, 'x64;'))) {
+            $sReturn = 'amd64';
+        }
+        return $sReturn;
+    }
+
+    private function detectX64ArchitectureByOs($usrA)
+    {
+        $identifiers = ['x86_64;', 'x86-64;', 'Win64;', 'x64;', 'amd64;', 'AMD64', 'WOW64', 'x64_64;'];
+        return $this->eVlByAltSubstrings($identifiers, $usrA, 'amd64');
+    }
+
+    private function detectX86Architecture($usrA)
+    {
+        $identifiers = ['i386', 'i586', 'ia32;', 'WOW64', 'x86;'];
+        return $this->eVlByAltSubstrings($identifiers, $usrA, 'ia32');
+    }
+
+    private function enrichParticularArrayValue($inSingleArray)
+    {
+        $outCompleteArray = [];
+        foreach ($inSingleArray as $keyS => $valueS) {
+            $trnkArray['name and bits'] = $valueS['name'] . ' (' . $valueS['bits'] . ' bit)';
+            $value2                     = array_merge($valueS, $trnkArray);
+            ksort($value2);
+            $outCompleteArray[$keyS]    = $value2;
+        }
+        return $outCompleteArray;
+    }
+
+    private function eVlByAltSubstrings($inArray, $inSubject, $vlFinal)
+    {
+        $sReturn          = '';
+        $regularExpresion = '/' . implode('|', $inArray) . '/';
+        if (preg_match($regularExpresion, $inSubject) === 1) {
+            $sReturn = $vlFinal;
+        }
+        return $sReturn;
+    }
+
     private function listOfCpuArchitecturesX86()
     {
-        return [
+        $lst = [
             'arm'  => [
-                'bits'          => 32,
-                'nick'          => 'ARM',
-                'name'          => 'Advanced RISC Machine',
-                'name and bits' => 'Advanced RISC Machine (32 bit)',
+                'nick' => 'ARM',
+                'name' => 'Advanced RISC Machine',
             ],
             'ia32' => [
-                'bits'          => 32,
-                'nick'          => 'x86',
-                'name'          => 'Intel x86',
-                'name and bits' => 'Intel x86 (32 bit)',
+                'nick' => 'x86',
+                'name' => 'Intel x86',
             ],
             'ppc'  => [
-                'bits'          => 32,
-                'name'          => 'Power PC',
-                'name and bits' => 'Power PC (32 bit)',
+                'nick' => 'PPC',
+                'name' => 'Power PC',
             ],
         ];
+        return $this->enrichParticularArrayValue($this->addStdandardTrunk($lst, ['bits' => 32]));
+    }
+
+    protected function getArchitectureFromUserAgentBrowser($usrA)
+    {
+        $knownArchitectures = $this->listOfKnownCpuArchitectures();
+        if ($this->detectX64Architecture($usrA) == 'ia32') {
+            $aReturn = $knownArchitectures['ia32'];
+        } elseif (strpos($usrA, 'Android;')) {
+            $aReturn = $knownArchitectures['arm'];
+        } elseif ($this->detectX64Architecture($usrA) == 'amd64') {
+            $aReturn = $knownArchitectures['amd64'];
+        } else {
+            $aReturn = $knownArchitectures['ia32'];
+        }
+        return $aReturn;
+    }
+
+    protected function getArchitectureFromUserAgentOperatingSystem($usrA)
+    {
+        $knowCpuArchitecture = $this->listOfKnownCpuArchitectures();
+        if ($this->detectX64ArchitectureByOs($usrA) == 'amd64') {
+            $aReturn = $knowCpuArchitecture['amd64'];
+        } elseif (strpos($usrA, 'Android;')) {
+            $aReturn = $knowCpuArchitecture['arm'];
+        } else {
+            $aReturn = $knowCpuArchitecture['ia32'];
+        }
+        return $aReturn;
     }
 
     private function listOfCpuArchitecturesX64()
     {
-        return [
+        $lst = [
             'amd64' => [
-                'bits'          => 64,
-                'nick'          => 'x64',
-                'name'          => 'AMD/Intel x64',
-                'name and bits' => 'AMD/Intel x64 (64 bit)',
+                'nick' => 'x64',
+                'name' => 'AMD/Intel x64',
             ],
             'arm64' => [
-                'bits'          => 64,
-                'nick'          => 'ARM64',
-                'name'          => 'Advanced RISC Machine',
-                'name and bits' => 'Advanced RISC Machine (64 bit)',
+                'nick' => 'ARM64',
+                'name' => 'Advanced RISC Machine',
             ],
             'ppc64' => [
-                'bits'          => 64,
-                'name'          => 'Power PC',
-                'name and bits' => 'Power PC (64 bit)',
+                'nick' => 'PPC64',
+                'name' => 'Power PC',
             ],
             'sun'   => [
-                'bits'          => 64,
-                'name'          => 'Sun Sparc',
-                'name and bits' => 'Sun Sparc (64 bit)',
+                'nick' => 'Sparc',
+                'name' => 'Sun Sparc',
             ],
         ];
+        return $this->enrichParticularArrayValue($this->addStdandardTrunk($lst, ['bits' => 64]));
     }
 
     /**
@@ -85,7 +160,7 @@ trait ArchiecturesCpu
      *
      * @return array
      */
-    protected function listOfKnownCpuArchitectures()
+    private function listOfKnownCpuArchitectures()
     {
         $arc = array_merge($this->listOfCpuArchitecturesX86(), $this->listOfCpuArchitecturesX64());
         ksort($arc);
