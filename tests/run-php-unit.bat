bcd6de65342effa64d767a0c758f011bb55804ce
Daniel Popiniuc <danielpopiniuc@gmail.com>
2018-06-24 02:11:24 +0300

    added ability to check PHP compatbility for various versions

--- tests/run-php-unit.bat
+++ tests/run-php-unit.bat
@@ -1,1 +1,7 @@
-C:\www\App\PHP\7.2.x64\php.exe -c C:\www\Config\PHP\7.2.x64+xDebug\php.ini D:\www\Data\GitRepositories\GitHub\danielgp\network-components\vendor\phpunit\phpunit\phpunit  --configuration D:\www\Data\GitRepositories\GitHub\danielgp\network-components\phpunit.xml
+@echo off
+SETLOCAL ENABLEEXTENSIONS
+SET me=%~n0
+SET current_folder=%~dp0
+SET parent_folder=%current_folder:\tests\=\%
+echo %parent_folder%
+C:\www\App\PHP\7.2.x64\php.exe -c C:\www\Config\PHP\7.2.x64+xDebug\php.ini %parent_folder%vendor\phpunit\phpunit\phpunit  --configuration %parent_folder%phpunit.xml
