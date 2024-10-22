07280b6e996709c896d66dcebe5b94c479011d39
Daniel Popiniuc <danielpopiniuc@gmail.com>
2024-10-23 02:04:59 +0300

    enhancing Curl class with typed method parameters and implement NoBody feature for scenarios where content is not needed

--- source/InputOutputCurl.php
+++ source/InputOutputCurl.php
@@ -29,26 +29,12 @@ namespace danielgp\io_operations;
 trait InputOutputCurl
 {
 
-    public function getContentFromUrlThroughCurl($fullURL, $features = [])
+    public function getContentFromUrlThroughCurl(string $fullURL, array $features = [])
     {
         $chanel     = curl_init();
-        $this->setUserAgent($chanel, $features);
         $inScopeUrl = $this->validateUrl($fullURL);
-        $this->handleSecureConnection($chanel, $inScopeUrl, $features);
-        $this->setForceIpv4($chanel, $features);
-        curl_setopt($chanel, CURLOPT_URL, $inScopeUrl);
-        curl_setopt($chanel, CURLOPT_HEADER, false);
-        $this->setPostingDetails($chanel, $features);
-        if (array_key_exists('HttpHeader', $features) && !array_key_exists('PostFields', $features)) {
-            curl_setopt($chanel, CURLOPT_HTTPHEADER, $features['HttpHeader']);
-        }
-        curl_setopt($chanel, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($chanel, CURLOPT_FOLLOWLOCATION, true);
-        curl_setopt($chanel, CURLOPT_MAXREDIRS, 10);
-        curl_setopt($chanel, CURLOPT_FRESH_CONNECT, true); //avoid a cached response
-        curl_setopt($chanel, CURLOPT_FAILONERROR, true);
-        curl_setopt($chanel, CURLOPT_TCP_FASTOPEN, true);
-        $aReturn = [
+        $this->handleCurlOptions($chanel, $inScopeUrl, $features);
+        $aReturn    = [
             'response' => curl_exec($chanel),
             'errNo'    => curl_errno($chanel),
             'errMsg'   => curl_error($chanel),
@@ -56,11 +42,34 @@ trait InputOutputCurl
         if (array_key_exists('includeStatistics', $features)) {
             $aReturn['info'] = curl_getinfo($chanel);
         }
+        if (array_key_exists('NoBody', $features)) {
+            unset($aReturn['response']);
+        }
         curl_close($chanel);
         return $aReturn;
     }
 
-    private function handleSecureConnection($chanel, $fullURL, $features)
+    private function handleCurlOptions(\CurlHandle|false $chanel, string $inScopeUrl, array $features): void
+    {
+        $this->setUserAgent($chanel, $features);
+        $this->handleSecureConnection($chanel, $inScopeUrl, $features);
+        $this->setForceIpv4($chanel, $features);
+        $this->setNoBody($chanel, $features);
+        curl_setopt($chanel, CURLOPT_FAILONERROR, true);
+        curl_setopt($chanel, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($chanel, CURLOPT_FRESH_CONNECT, true); //avoid a cached response
+        curl_setopt($chanel, CURLOPT_HEADER, false);
+        curl_setopt($chanel, CURLOPT_MAXREDIRS, 10);
+        curl_setopt($chanel, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($chanel, CURLOPT_TCP_FASTOPEN, true);
+        curl_setopt($chanel, CURLOPT_URL, $inScopeUrl);
+        $this->setPostingDetails($chanel, $features);
+        if (array_key_exists('HttpHeader', $features) && !array_key_exists('PostFields', $features)) {
+            curl_setopt($chanel, CURLOPT_HTTPHEADER, $features['HttpHeader']);
+        }
+    }
+
+    private function handleSecureConnection(\CurlHandle|false $chanel, string $fullURL, array $features): void
     {
         if ((substr(strtolower($fullURL), 0, 5) === 'https')) {
             $chk = false;
@@ -72,7 +81,7 @@ trait InputOutputCurl
         }
     }
 
-    private function setForceIpv4($chanel, $features)
+    private function setForceIpv4(\CurlHandle|false $chanel, array $features): void
     {
         if (array_key_exists('forceIpV4', $features)) {
             if (defined('CURLOPT_IPRESOLVE') && defined('CURL_IPRESOLVE_V4')) {
@@ -81,7 +90,14 @@ trait InputOutputCurl
         }
     }
 
-    private function setPostingDetails($chanel, $features)
+    private function setNoBody(\CurlHandle|false $chanel, array $features): void
+    {
+        if (array_key_exists('NoBody', $features)) {
+            curl_setopt($chanel, CURLOPT_NOBODY, true);
+        }
+    }
+
+    private function setPostingDetails(\CurlHandle|false $chanel, array $features): void
     {
         if (array_key_exists('PostFields', $features)) {
             curl_setopt($chanel, CURLOPT_POST, true);
@@ -90,14 +106,14 @@ trait InputOutputCurl
         }
     }
 
-    private function setUserAgent($chanel, $features)
+    private function setUserAgent(\CurlHandle|false $chanel, array $features): void
     {
         if (array_key_exists('setUserAgent', $features)) {
             curl_setopt($chanel, CURLOPT_USERAGENT, $features['setUserAgent']);
         }
     }
 
-    private function validateUrl($strUrl)
+    private function validateUrl(string $strUrl): string
     {
         $inScopeUrl = filter_var($strUrl, FILTER_VALIDATE_URL);
         if ($inScopeUrl === false) {
