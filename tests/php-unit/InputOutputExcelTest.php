d5541d7b3d8b0fcc4c44851461f5a5fb650c10d5
Daniel Popiniuc <danielpopiniuc@gmail.com>
2025-06-01 02:06:02 +0300

    Update LICENSE
    switch from Eclipse 2.0 to 1.0

--- tests/php-unit/InputOutputExcelTest.php
+++ tests/php-unit/InputOutputExcelTest.php
@@ -1,42 +1,26 @@
 <?php
 
-/**
+/*
+ * Copyright (c) 2018 - 2025 Daniel Popiniuc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * The MIT License (MIT)
- *
- * Copyright (c) 2015 - 2024 Daniel Popiniuc
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *
+ * Contributors:
+ *    Daniel Popiniuc
  */
 
 namespace danielgp\io_operations;
 
 class InputOutputExcelTest extends \PHPUnit\Framework\TestCase
 {
-
     protected function setUp()
     {
         require_once str_replace('tests' . DIRECTORY_SEPARATOR . 'php-unit', 'vendor', __DIR__)
-                . DIRECTORY_SEPARATOR . 'autoload.php';
+            . DIRECTORY_SEPARATOR . 'autoload.php';
         require_once str_replace('tests' . DIRECTORY_SEPARATOR . 'php-unit', 'source', __DIR__)
-                . DIRECTORY_SEPARATOR . 'InputOutputExcel.php';
+            . DIRECTORY_SEPARATOR . 'InputOutputExcel.php';
     }
 
     /**
@@ -174,7 +158,7 @@ class InputOutputExcelTest extends \PHPUnit\Framework\TestCase
     {
         $mock                = $this->getMockForTrait(InputOutputExcel::class);
         $strFileNameWithPath = str_replace('tests' . DIRECTORY_SEPARATOR . 'php-unit', '', __DIR__)
-                . DIRECTORY_SEPARATOR . 'test_results.xlsx';
+            . DIRECTORY_SEPARATOR . 'test_results.xlsx';
         $mock->setArrayToExcel([
             'Filename'   => $strFileNameWithPath,
             'Worksheets' => [
--- tests/php-unit/php-unit--bootstrap.php
+++ tests/php-unit/php-unit--bootstrap.php
@@ -1,33 +1,18 @@
 <?php
 
-/**
+/*
+ * Copyright (c) 2018 - 2025 Daniel Popiniuc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * The MIT License (MIT)
- *
- * Copyright (c) 2015 - 2024 Daniel Popiniuc
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *
+ * Contributors:
+ *    Daniel Popiniuc
  */
 session_start();
 
-$_SERVER['HTTP_USER_AGENT']      = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:39.0) Gecko/20100101 Firefox/39.0';
+$_SERVER['HTTP_USER_AGENT']      = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:139.0) Gecko/20100101 Firefox/139.0';
 $_SERVER['HTTP_ACCEPT']          = 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8';
 $_SERVER['HTTP_ACCEPT_CHARSET']  = '';
 $_SERVER['HTTP_ACCEPT_ENCODING'] = 'gzip, deflate';
